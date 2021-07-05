055246d5ab0e1fc6303ef83277ed4286c4f19a6f
David Ramirez <davo.rmz@gmail.com>
2021-07-05 09:50:13 -0500

    cache management added

--- src/index.js
+++ src/index.js
@@ -15,3 +15,10 @@ const player = new MediaPlayer({
 });
 btnPlay.onclick = () => player.togglePlay();
 btnMute.onclick = () => player.toggleMute();
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+    .catch(error => {
+        console.error(error.message);
+    })
+}
---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_VERSION = 'v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(precache());
+});
+
+async function precache(){
+    const cache = await caches.open(CACHE_VERSION);
+    return cache.addAll([
+        '/',
+        '/index.html',
+        '/src/index.js',
+        '/src/js/MediaPlayer.js',
+        '/src/js/pluggins/AutoPlay.js',
+        '/src/js/pluggins/AutoPause.js',
+        '/src/styles/main.css',
+        '/assets/BigBuckBunny.mp4'
+    ]);
+}
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    if(request.method !== 'GET'){
+        return;
+    }
+
+    // look in cache
+    event.respondWith(cachedResponse(request));
+
+    //cache and network
+    event.waitUntil(updateCache(request));
+});
+
+async function cachedResponse(request){
+    const cache = await caches.open(CACHE_VERSION);
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request){
+    const cache = await caches.open(CACHE_VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response);
+}
+
