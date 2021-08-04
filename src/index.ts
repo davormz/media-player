98ffe08672954b96b910fe52f1f856dbcca2d94a
David Ramirez <davo.rmz@gmail.com>
2021-08-04 06:54:23 -0500

    icons added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import AutoPlay from './js/pluggins/AutoPlay';
 import AutoPause from './js/pluggins/AutoPause';
 import Ads from './js/pluggins/ads';
 
+const mediaContainer: HTMLElement = document.querySelector(".media-container");
 const video = document.querySelector("video");
 const btnPlay: HTMLElement = document.querySelector(".video-player__play");
 const btnMute: HTMLElement = document.querySelector(".video-player__mute");
@@ -11,10 +12,12 @@ const player = new MediaPlayer({
     el:video,
     pluggins: [
         new AutoPlay(),
-        new AutoPause(),
-        new Ads()
+        new AutoPause()
+        // ,
+        // new Ads()
     ]
 });
+video.onclick = () => player.togglePlay();
 btnPlay.onclick = () => player.togglePlay();
 btnMute.onclick = () => player.toggleMute();
 
